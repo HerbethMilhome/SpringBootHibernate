571e4ca114e874cc24a7084e638e739dd9f2af88
Herbeth Milhome <h2milhome@gmal.com>
2022-01-09 17:30:10 -0300

    add service

--- src/main/java/com/milhome/curso/resource/UserResource.java
+++ src/main/java/com/milhome/curso/resource/UserResource.java
@@ -1,19 +1,33 @@
 package com.milhome.curso.resource;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.milhome.curso.entities.User;
+import com.milhome.curso.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "maria", "email@email.com", "32132121", "12345");		
+	public ResponseEntity<List<User>> findAll(){
+		List<User> lista = service.findAll();
+		return ResponseEntity.ok().body(lista);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User u = service.findByID(id);
 		return ResponseEntity.ok().body(u);
 	}
 
