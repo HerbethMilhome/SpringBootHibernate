4f88b0f8ea297920783428dba94a56587997ab45
Herbeth Milhome <h2milhome@gmal.com>
2022-01-09 15:57:28 -0300

    add user e resourse

---
+++ src/main/java/com/milhome/curso/resource/UserResource.java
@@ -0,0 +1,20 @@
+package com.milhome.curso.resource;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.milhome.curso.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "maria", "email@email.com", "32132121", "12345");		
+		return ResponseEntity.ok().body(u);
+	}
+
+}
