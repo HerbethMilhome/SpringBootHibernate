6d0fdcc99918b90c523fc2550724f3526b9a1de8
Herbeth Milhome <h2milhome@gmal.com>
2022-01-16 09:58:47 -0300

    add exception

--- src/main/java/com/milhome/curso/services/UserService.java
+++ src/main/java/com/milhome/curso/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.milhome.curso.entities.User;
 import com.milhome.curso.repositories.UserRepository;
+import com.milhome.curso.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findByID(Long id) {
 		Optional<User> user = repository.findById(id);
-		return user.get();
+		return user.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User user) {
