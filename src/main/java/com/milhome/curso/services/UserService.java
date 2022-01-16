946d0cf9ff3e1f85dd19b7c16a54c149c2f37659
Herbeth Milhome <h2milhome@gmal.com>
2022-01-16 10:17:54 -0300

    add exception update

--- src/main/java/com/milhome/curso/services/UserService.java
+++ src/main/java/com/milhome/curso/services/UserService.java
@@ -3,6 +3,8 @@ package com.milhome.curso.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -43,9 +45,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User user) {
-		User entidade = repository.getById(id);
-		updateData(entidade, user);
-		return repository.save(entidade);
+		try {
+			User entidade = repository.getById(id);
+			updateData(entidade, user);
+			return repository.save(entidade);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entidade, User user) {
